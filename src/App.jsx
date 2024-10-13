30d0592534e3382e42873cc6bc95bc87c82ebd8b
Elias Hagos <eliasars@yahoo.com>
2024-10-14 00:55:00 +0300

    Minor updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,53 @@
-import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './components/Home';
+
+
 
 import './App.css';
-import Home from './components/Home';
-import { Contact } from './components/Contact';
-import { Project } from './components/Project';
 import About from './components/About';
+import Footer from './components/Footer';
+import Contact from './components/Contact';
+import Project from './components/Project';
 
 function App() {
   return (
     <Router>
-       <div>
-        <BrowserRouter>
-        <Navbar />
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark" style={{ backgroundColor: 'darkslategrey' }}>
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#"><i>Personal Portfolio</i></a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/">Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/contact">Contact</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/project">Project</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/about">About Me</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        <div className="container mt-4">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/project" element={<Project />} />
             <Route path="/about" element={<About />} />
           </Routes>
-        </BrowserRouter>
-
         </div>
+        <Footer />
+      </div>
     </Router>
   );
 }
