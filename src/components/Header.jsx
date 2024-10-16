e88da78dbbc37fe7a2ab5374b9c05197db09c05a
Elias Hagos <eliasars@yahoo.com>
2024-10-16 23:54:17 +0300

    New component added

---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export const Header = () => {
+  return (
+    <div>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark" style={{ backgroundColor: 'hsl(22, 18%, 30%)' }}>
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#"><i>Personal Portfolio</i></a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/contact">Contact</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/project">Project</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">About Me</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    </div>
+  )
+}
+
+
