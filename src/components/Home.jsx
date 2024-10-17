a3d3b9242e261197126391791fd66dd24bc96fa5
Elias Hagos <eliasars@yahoo.com>
2024-10-17 20:36:27 +0300

    UI of the Home component improved

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import DOMPurify from "dompurify"; 
+import DOMPurify from "dompurify";
 
 function Home() {
   const [homes, setHomes] = useState([]);
@@ -25,17 +25,23 @@ function Home() {
   if (error) return <div>Error loading home data: {error.message}</div>;
 
   return (
-    <div className="Home">
-      {homes.map((home) => (
-        <section key={home.id}>
-          <h2>{home.attributes.title}</h2>
-          <div
-            dangerouslySetInnerHTML={{
-              __html: DOMPurify.sanitize(home.attributes.body.processed),
-            }}
-          />
-        </section>
-      ))}
+    <div className="project-wrapper">
+      <div className="container py-5">
+        <div className="row">
+          <div className="col-lg-8 col-md-10 mx-auto">
+            {homes.map((home) => (
+              <section key={home.id}>
+                <h2>{home.attributes.title}</h2>
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: DOMPurify.sanitize(home.attributes.body.processed),
+                  }}
+                />
+              </section>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
